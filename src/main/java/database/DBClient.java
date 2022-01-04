6e5631a4490bfa8e64bf2d8f0db29b48523cddfe
JiahuiChen99 <Jiahui1@hotmail.es>
2022-01-04 16:44:37 +0100

    feat: db client singleton with sytem environment variables

--- src/main/java/database/DBClient.java
+++ src/main/java/database/DBClient.java
@@ -3,6 +3,7 @@ package database;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Objects;
 
 
 /**
@@ -10,8 +11,8 @@ import java.sql.SQLException;
  * It uses the Singleton pattern
  */
 public class DBClient {
-    private String user = "";
-    private String psw = "";
+    private String user = "root";
+    private String psw = "root";
     private final String db_path = "jdbc:mysql://localhost:3306/pizzisalle";    // Localhost MySQL default port - pizzisalle DB
     private static Connection connection;
 
@@ -19,13 +20,31 @@ public class DBClient {
 
     // Creates a MySQL connection
     public DBClient() {
-        // TODO: Load environment variables
+        // Load environment variables
+        // For testing purposes 'user' & 'psw' are set directly
+        // loadDBEnvironment();
+
         // Get a MySQL client connection
         try {
+            Class.forName("com.mysql.jdbc.Driver");
             connection = DriverManager.getConnection(this.db_path, this.user, this.psw);
             if ( connection == null ) System.exit(-1);
-        } catch (SQLException e) {
+        } catch (SQLException | ClassNotFoundException e) {
             e.printStackTrace();
+        }
+    }
+
+    /**
+     * Loads system environment variables.
+     * These will be used to get a connection to the DB,
+     * hence an instance of DBClient
+     */
+    private void loadDBEnvironment() {
+        this.user = System.getenv("PIZZI_DB_USER");
+        this.psw = System.getenv("PIZZI_DB_PSW");
+        if (Objects.equals(this.user, "") || Objects.equals(this.psw, "")) {
+            System.out.println("Either PIZZI_DB_USER or PIZZI_DB_PSW System environment variables are not set up. Please configure it");
+            System.exit(-1);
         }
     }
 
