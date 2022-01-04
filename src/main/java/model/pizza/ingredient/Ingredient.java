ecc17191f2388527a6863ef331a947fff53e7de6
JiahuiChen99 <Jiahui1@hotmail.es>
2022-01-04 11:14:10 +0100

    feat: ingredient class & types enum

---
+++ src/main/java/model/pizza/ingredient/Ingredient.java
@@ -0,0 +1,38 @@
+package model.pizza.ingredient;
+
+public class Ingredient {
+    private int ID;
+    private String name;
+    private String description;
+
+    public Ingredient(int ID, String name, String description) {
+        this.ID = ID;
+        this.name = name;
+        this.description = description;
+    }
+
+    // Getters & Setters
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
