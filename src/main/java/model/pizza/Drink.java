6a829d7f070a174c74078b03bf37a10c04931994
JiahuiChen99 <Jiahui1@hotmail.es>
2022-01-04 10:38:01 +0100

    feat: drink class

---
+++ src/main/java/model/pizza/Drink.java
@@ -0,0 +1,37 @@
+package model.pizza;
+
+public class Drink {
+    private int ID;
+    private String name;
+
+    // Drink Constructors
+    public Drink(int ID, String name) {
+        this.ID = ID;
+        this.name = name;
+    }
+
+    public Drink(int ID) {
+        this.ID = ID;
+    }
+
+    public Drink(String name) {
+        this.name = name;
+    }
+
+    // Getters & Setters
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
