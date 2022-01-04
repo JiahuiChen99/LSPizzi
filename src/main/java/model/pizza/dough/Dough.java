6919ea5671320ee972411da38ff91e28bcd4ace9
JiahuiChen99 <Jiahui1@hotmail.es>
2022-01-04 10:49:01 +0100

    feat: dough class & types enum

---
+++ src/main/java/model/pizza/dough/Dough.java
@@ -0,0 +1,37 @@
+package model.pizza.dough;
+
+public class Dough {
+    private int ID;
+    private String name;
+
+    // Drink Constructors
+    public Dough(int ID, String name) {
+        this.ID = ID;
+        this.name = name;
+    }
+
+    public Dough(int ID) {
+        this.ID = ID;
+    }
+
+    public Dough(String name) {
+        this.name = name;
+    }
+
+    // Getters & Setters
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/model/pizza/dough/DoughType.java
@@ -0,0 +1,10 @@
+package model.pizza.dough;
+
+/**
+ * Enumeration defining the types of Dough for the pizza
+ */
+public enum DoughType {
+    ORIGINAL,   // Default if not specified
+    THIN,
+    THICK
+}
