3fe5604e29a13cded7f41bc73b800f0b0147b11c
JiahuiChen99 <Jiahui1@hotmail.es>
2022-01-04 17:08:10 +0100

    feat: order terminal based view

---
+++ src/main/java/view/OrderForm.java
@@ -0,0 +1,31 @@
+package view;
+
+public class OrderForm {
+    public void welcome() {
+        System.out.println("###############################################");
+        System.out.println("Welcome to LSPizzi");
+        System.out.println("###############################################");
+    }
+
+    public void newOrder() {
+        System.out.println("###############################################");
+        System.out.println("Create a new order");
+        System.out.println("###############################################");
+    }
+
+    public void doughs() {
+        System.out.println("###############################################");
+        System.out.println("Available doughs");
+        System.out.println("###############################################");
+
+        // TODO: Print doughs
+    }
+
+    public void drinks() {
+        System.out.println("###############################################");
+        System.out.println("Available drinks");
+        System.out.println("###############################################");
+
+        // TODO: Print drinks
+    }
+}
