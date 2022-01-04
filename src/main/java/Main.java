b8c7dd21c0d33926d6d4005b70524b687b03098f
JiahuiChen99 <Jiahui1@hotmail.es>
2022-01-04 16:57:49 +0100

    feat: basic main layout

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,16 @@
+import database.DBClient;
+
 public class Main {
+
+    /**
+     * The application uses Model-View-Controller (MVC)
+     */
     public static void main(String[] args) {
 
+        // Get DB client singleton
+        DBClient dbClient = DBClient.getConnection();
+
+        // Close DB client singleton
+        dbClient.closeConnection();
     }
 }
