8bf65a2dac3b13c73fa3bd28d0c69df56cad8f72
JiahuiChen99 <Jiahui1@hotmail.es>
2022-01-04 17:13:57 +0100

    docs: different software design patterns

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,16 @@ import database.DBClient;
 public class Main {
 
     /**
-     * The application uses Model-View-Controller (MVC)
+     * The user must ensure to run the pizzisalle.sql DB
+     * script before running the application.
+     *
+     * This project implements the following software design
+     * patterns:
+     *
+     * - MVC: The application itself uses this pattern abstract
+     *      different layers.
+     * - Singleton: For the DB client, one and only instance
+     * - Builder: To customize and build the pizza.
      */
     public static void main(String[] args) {
 
