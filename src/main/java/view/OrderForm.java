50475e20aa3fb165949f6bf9faafa6ce761a6673
JiahuiChen99 <Jiahui1@hotmail.es>
2022-01-05 20:43:56 +0100

    refactor: styled delegations view

--- src/main/java/view/OrderForm.java
+++ src/main/java/view/OrderForm.java
@@ -29,10 +29,10 @@ public class OrderForm {
         System.out.println("Which restaurant");
         System.out.println("###############################################");
         System.out.println("First choose where you want to place the order (Delegation)");
-        System.out.println("\t[1]. Barcelona");
-        System.out.println("\t[2]. Lleida");
-        System.out.println("\t[3]. Tarragona");
-        System.out.println("\t[4]. Girona");
+        System.out.println("\t[1]- Barcelona");
+        System.out.println("\t[2]- Lleida");
+        System.out.println("\t[3]- Tarragona");
+        System.out.println("\t[4]- Girona");
         return customerChoice();
     }
 
