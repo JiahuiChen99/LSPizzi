e13eea6886e84ed001ac77293d90f9cec3e47a3d
JiahuiChen99 <Jiahui1@hotmail.es>
2022-01-05 17:02:12 +0100

    feat: scanner for user input and improved order form

--- src/main/java/view/OrderForm.java
+++ src/main/java/view/OrderForm.java
@@ -1,16 +1,39 @@
 package view;
 
+import java.util.Scanner;
+
 public class OrderForm {
-    public void welcome() {
+    private Scanner customerIput;
+
+    public OrderForm() {
+        // Create a new input scanner
+        this.customerIput = new Scanner(System.in);
+    }
+
+    private String customerChoice() {
+        return this.customerIput.nextLine();
+    }
+
+    public String welcome() {
         System.out.println("###############################################");
         System.out.println("Welcome to LSPizzi");
         System.out.println("###############################################");
+        System.out.println("\t[1]- Place an order");
+        System.out.println("\t[2]- Exit");
+        System.out.println("Select an option:");
+        return customerChoice();
     }
 
-    public void newOrder() {
+    public String newOrder() {
         System.out.println("###############################################");
         System.out.println("Create a new order");
         System.out.println("###############################################");
+        System.out.println("First choose where you want to place the order (Delegation)");
+        System.out.println("\t[1]. Barcelona");
+        System.out.println("\t[1]. Lleida");
+        System.out.println("\t[1]. Tarragona");
+        System.out.println("\t[1]. Girona");
+        return customerChoice();
     }
 
     public void doughs() {
